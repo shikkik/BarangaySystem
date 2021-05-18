964bf7d8b284d75f6def440f1de2a85b18bf7c49
Shikkik <Soufuuuu@gmail.com>
2021-05-18 01:51:42 -0700

    maraming mga binago na file

--- resident_profile.php
+++ resident_profile.php
@@ -1,9 +1,17 @@
 <?php 
     require('classes/resident.class.php');
 
-    $view = $residentbmis->view_single_resident($email);
+    //$view = $residentbmis->view_single_resident($email);
     $userdetails = $residentbmis->get_userdata();
-    
+    $view = $residentbmis->view_household_list();
+
+    //$lname = $_GET['lname'];
+    //$lname = $_GET['mi'];
+
+    //$connection = $residentbmis->openConn();
+    //$stmt = $connection->prepare("SELECT * FROM tbl_resident WHERE lname ='$lname'");
+    //$stmt->execute();  
+   // $view = $stmt->fetch();
     
 ?>
 
@@ -12,6 +20,9 @@
 
     <head> 
     <title> Barangay Management System </title>
+        <script src="../BarangaySystem/customjs/main.js" type="text/javascript"> </script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <!-- responsive tags for screen compatibility -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- custom css --> 
@@ -29,63 +40,84 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="navbar-nav ml-auto">
-                <a class="nav-item nav-link" href="resident_profile.php?ID=<?php echo $profile['id_resident'];?>"> view </a>
-                <a class="nav-item nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i></a>
+            <div class="dropdown ml-auto">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><?= $userdetails['surname'];?>, <?= $userdetails['firstname'];?>
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                    <button class="btn" onclick="profile();"> <i class="fas fa-user" style="padding: 0.5em;"></i>Personal Profile  </button>
+                    <button class="btn" onclick="logout();"> <i class="fas fa-sign-out-alt" style="padding: 0.5em;"></i> Logout  </button>
+                </ul>
             </div>
         </nav>
+
+
 
         <div class="container"  style="margin-top: 4em;">
             <div class="row">
                 <div class="col-12">
                     <h1 class="text-center">Resident Profile</h1>
-                    
-
-                <div class="row margin mtop"> 
-                    <div class="col-sm"> </div>
-                    <div class="col-12">   
-                        <div class="card mbottom">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col">
-                                    <h5> Name: <?= $userdetails['surname'];?>, <?= $userdetails['firstname'];?> <?= $userdetails['mname'];?>.</h5> 
-                                    <h5> Email: <?= $userdetails['emailadd'];?> </h5>
-                                    <h5> Sex: <?= $userdetails['sex'];?> </h5>
-                                    <h5> Status: <?= $userdetails['status'];?> </h5>
-                                    <h5> Address: <?= $userdetails['address'];?> </h5>
-                                    <h5> Contact: <?= $userdetails['contact'];?> </h5>
-                                    <h5> Birth Date: <?= $userdetails['bdate'];?> </h5>
-                                    <h5> Birth Place: <?= $userdetails['bplace'];?> </h5>
-                                    <h5> Nationality: <?= $userdetails['nationality'];?> </h5>
-                                    <h5> Family Role: <?= $userdetails['family_role'];?> </h5>
-                                </div>   
-                            </div>
-                        </div>
-                    </div> 
-
-                    <div class="col-sm"> 
-                        <form method="POST" action="">
-                        <div class="form-inline">
-                            <input  class="form-control" name="mname" type="hidden" value="<?= $userdetails['mname'];?>" placeholder="Search here..." required=""/>
-                            <input  class="form-control" name="surname" type="hidden" value="<?= $userdetails['surname'];?>" placeholder="Search here..." required=""/>
-                            <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo" name="search">View Household</button>
-                            <div id="demo" class="collapse">
-                                <br /><br />
-                                <?php include'testingsearch.php'?>
-                                                
-                            </div>
-                        </div>                                   
-                        </form>
-                    </div>
+                    <hr>
                 </div>
             </div>
+
+        
+            <div class="row margin mtop"> 
+                <div class="col-sm"> </div>
+                <div class="col-12">   
+                    <div class="card mbottom">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h5> Name <?= $userdetails['surname'];?>, <?= $userdetails['firstname'];?> <?= $userdetails['mname'];?>.</h5> 
+                                    <h5> Email <?= $userdetails['emailadd'];?> </h5>
+                                    <h5> Sex <?= $userdetails['sex'];?> </h5>
+                                    
+                                    <input type="text" name="address" placeholder="<?= $userdetails['status'];?>">
+                                    <br>
+                                    <input type="text" name="address" placeholder="<?= $userdetails['address'];?>">
+                                    <br>
+                                    <input type="text" name="address" placeholder="<?= $userdetails['contact'];?>">
+    
+
+                                    <h5> Birth Date <?= $userdetails['bdate'];?> </h5>
+                                    <h5> Birth Place <?= $userdetails['bplace'];?> </h5>
+                                    <h5> Nationality <?= $userdetails['nationality'];?> </h5>
+                                    <h5> Family Role <?= $userdetails['family_role'];?> </h5>
+                                </div>
+
+                                <div class="col"></div>
+                            </div>  
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="col-sm"> </div>
+            </div>
+
+            <div class="row" style="margin-top: 3em; margin-bottom: 5em;"> 
+                <div class="col-xl-12">
+                    <div class="form-inline">
+                    <form method="post">
+                            <input class="form-control" name="lname" type="hidden" value="<?= $userdetails['surname'];?>"/>
+                            <input class="form-control" name="mi" type="hidden" value="<?= $userdetails['mname'];?>" />
+                            <button type="submit button" class="btn btn-info"  name="search_household">View Household</button>
+                        <div>
+                        <br /><br />
+                            <?php include'testingsearch.php'?>
+                        </div>
+                    </form>
+                    </div>                                   
+                </div>
+            </div>
+
+           
+            
+            
+  
         </div>
 
-        
-        
-        </section>
-
         <script src="../BarangaySystem/bootstrap/js/bootstrap.bundle.js" type="text/javascript"> </script>
-         
+        
     </body>
 </html>
