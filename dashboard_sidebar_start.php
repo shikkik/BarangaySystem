2a926fc13778fe9ea9455560f3334f84f8465c19
PJ <pjmendros12@gmail.com>
2021-06-12 12:03:58 +0800

    dashboard ulit

--- dashboard_sidebar_start.php
+++ dashboard_sidebar_start.php
@@ -16,10 +16,10 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-
+    
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
+    <script src="https://kit.fontawesome.com/67a9b7069e.js" crossorigin="anonymous"></script>
 </head>
 
 <body id="page-top">
